e23984a1998bcb82856e4a9ff6dbaf987e5a9391
2bcordeliers <jung.jules+2bcordeliers@pm.me>
2024-12-12 21:18:01 +0000

    add basic list view

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-(async function () {
+let homeworksList = document.querySelector("ul#homeworks-list");
+
+async function loadData() {
     let response = await fetch("data.json");
     let data = await response.json();
-    console.log(data);
+    return [data.subjects, data.homeworks];
+}
+
+(async function () {
+    let [subjects, homeworks] = await loadData();
+    for (let homework of homeworks) {
+        let subject = subjects.find(subject => subject.id === homework.subject_id);
+        let homeworkListItem = document.createElement("li");
+        homeworkListItem.innerText = `[${subject.name}]: ${homework.description}`;
+        homeworksList.appendChild(homeworkListItem);
+    }
+    console.log(subjects, homeworks);
 })();
