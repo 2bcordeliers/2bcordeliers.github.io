c5c04eb79f313033c52fb723cc0013d12a8643a7
2bcordeliers <191517390+2bcordeliers@users.noreply.github.com>
2025-01-25 23:46:08 +0100

    fix: add tests

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,21 @@
 import { defineCollection, z } from "astro:content";
 
+const subjects = z.enum([
+  "Vie de classe",
+  "Français",
+  "Maths",
+  "Histoire-Géo",
+  "Anglais",
+  "Physique-Chimie",
+  "SVT",
+  "SNT",
+]);
+
 const homeworks = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    subject: z.enum([
-      "Vie de classe",
-      "Français",
-      "Maths",
-      "Histoire-Géo",
-      "Anglais",
-      "Physique-Chimie",
-      "SVT",
-      "SNT",
-    ]),
+    subject: subjects,
     date: z.date(),
     lessons: z.array(z.string()).optional(),
   }),
@@ -26,4 +28,14 @@ const lessons = defineCollection({
   }),
 });
 
-export const collections = { homeworks, lessons };
+const tests = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    subject: subjects,
+    date: z.date(),
+    lessons: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { homeworks, lessons, tests };
