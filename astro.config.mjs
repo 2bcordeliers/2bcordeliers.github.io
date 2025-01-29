f749e8ce058a5d51336826906d31f822e5dc8834
2bcordeliers <191517390+2bcordeliers@users.noreply.github.com>
2025-01-29 22:05:39 +0100

    feat(markdown): add latex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,15 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://2bcordeliers.github.io",
   base: "/",
+  markdown: {
+    remarkPlugins: ["remark-math"],
+    rehypePlugins: [
+      [
+        "rehype-katex",
+        {
+          trust: true,
+        },
+      ],
+    ],
+  },
 });
